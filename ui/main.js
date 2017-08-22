bb1631d2710969b8e3b14d1b5981cfff72948dc4
RonyJohnson <31036505+RonyJohnson@users.noreply.github.com>
2017-08-22 17:16:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 // make changes on main-text div
 
-var element=document.getelementbyid('main-text');
+var element=document.getElementById('main-text');
 
 element.innerHTML='RONY';
 
